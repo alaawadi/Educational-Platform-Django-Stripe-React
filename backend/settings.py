53812cbc8645fe37fc46b649254295f9d103c58a
Alaa Wadi <84800142+alaawadi@users.noreply.github.com>
2022-10-13 14:20:53 +0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -198,7 +198,7 @@ STRIPE_WEBHOOK_SECRET = "whsec_VV1cfD0WOqnGPzHKBYRnnEJ0z0g4I0Am"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'alaawadi98@gmail.com'
-EMAIL_HOST_PASSWORD = 'rwqlznpzjebmtgib'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = '*******'
 EMAIL_USE_TLS = True
 EMAIL_PORT = '587'
